25fa0bfd1f199ba6f0aa4e85581370ef566801ac
Aurimar Lopes <aurimardev@gmail.com>
2025-03-05 04:37:06 +0000

    feat: enhance metadata configuration with additional SEO properties

--- src/config/metadata.ts
+++ src/config/metadata.ts
@@ -3,4 +3,15 @@ import type { Metadata } from "next";
 export const CustomMetadata: Metadata = {
   title: "Aurimar Lopes",
   description: "Portfolio Pessoal do Aurimar Lopes",
+  keywords: ["Aurimar Lopes", "Aurimar", "Lopes", "Portfolio", "Personal", "Personal Portfolio"],
+  authors: [
+    {
+      name: "Aurimar Lopes",
+      url: "https://aurimarlopes.com",
+    }
+  ]
+  ,
+  applicationName: "Aurimar",
+  viewport: "width=device-width, initial-scale=1.0",
+  publisher: "Aurimar Lopes",
 };
